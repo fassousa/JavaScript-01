9cf53f8cd7bbceafd40bf45157298f1cf0f4a398
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-14 20:50:08 -0300

    Basic Todo list setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,17 @@
 //= require rails-ujs
 //= require_tree .
+var inputEl = document.querySelector('input');
+var buttonEl = document.querySelector('button');
+var ulEl = document.querySelector('ul');
+
+buttonEl.addEventListener('click', addTodo);
+
+function addTodo() {
+  var userInput = inputEl.value;
+  if (userInput.trim() == '') {
+    return
+  }
+  var todoLi = document.createElement('LI');
+  todoLi.textContent = userInput;
+  ulEl.appendChild(todoLi);
+}
