23ba0c7ab5d9afaac874a4b1a4f79c836c3f4831
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-14 22:23:49 -0300

    Todo list prototype functional

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,8 @@ var inputEl = document.querySelector('input');
 var buttonEl = document.querySelector('button');
 var ulEl = document.querySelector('ul');
 
+var todos = [];
+
 buttonEl.addEventListener('click', addTodo);
 
 function addTodo() {
@@ -11,13 +13,25 @@ function addTodo() {
   if (userInput.trim() == '') {
     return
   }
+  var newTodo = { id: Math.random(), value: userInput };
+  todos.push(newTodo);
   var todoLi = document.createElement('LI');
   todoLi.textContent = userInput;
   todoLi.addEventListener('click', removeTodo);
+  todoLi.dataset.id = newTodo.id;
   ulEl.appendChild(todoLi);
+  console.log(todos);
 }
 
 function removeTodo() {
   var clickedLi = event.target;
+  var itemId = clickedLi.dataset.id;
+  for (var i = todos.length - 1; i >= 0; i--) {
+    if (todos[i].id == itemId) {
+      todos.splice(i, 1);
+      break;
+    }
+  }
   clickedLi.parentNode.removeChild(clickedLi);
+  console.log(todos);
 }
