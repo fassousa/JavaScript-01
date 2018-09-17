79645c5fcad65e4eb660d59a519ccfa0f316c9a1
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-16 22:51:32 -0300

    Vue JS Basic app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,31 +1,39 @@
 //= require rails-ujs
 //= require_tree .
+
+// new Vue({
+//   el: '#app',
+//   data: {
+//     todos: [],
+//     userInput: ''
+//   },
+//   methods: {
+//     addTodo: function() {
+//       if (this.userInput.trim() == '') {
+//         return;
+//       }
+//       var newTodo = {
+//         id: _.uniqueId(),
+//         value: this.userInput
+//       };
+//       this.todos.push(newTodo);
+//       console.log(this.todos);
+//     },
+//     removeTodo(todo) {
+//       var id = todo.id;
+//       for (var i = 0; i < this.todos.lenght; i++) {
+//         if (this.todos[i].id == id) {
+//           this.todos.splice(i, 1);
+//           break;
+//         }
+//       }
+//     }
+//   }
+// });
+
 new Vue({
   el: '#app',
   data: {
-    todos: [],
-    userInput: ''
-  },
-  methods: {
-    addTodo: function() {
-      if (this.userInput.trim() == '') {
-        return;
-      }
-      var newTodo = {
-        id: _.uniqueId(),
-        value: this.userInput
-      };
-      this.todos.push(newTodo);
-      console.log(this.todos);
-    },
-    removeTodo(todo) {
-      var id = todo.id;
-      for (var i = 0; i < this.todos.lenght; i++) {
-        if (this.todos[i].id == id) {
-          this.todos.splice(i, 1);
-          break;
-        }
-      }
-    }
+    name: 'Max'
   }
 });
